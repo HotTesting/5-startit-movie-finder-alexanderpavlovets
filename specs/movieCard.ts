beb5bce1da4d1cfd6f847e3632a03a8c290cd6ad
Alexander Pavlovets <sam-90@bigmir.net>
2018-01-31 13:01:58 +0200

    Home work from lesson 3 template

---
+++ specs/movieCard.ts
@@ -0,0 +1,16 @@
+import { browser, $, $$, element, by} from 'protractor'
+
+describe('Movie card ', async function(){
+
+    it('should have name', async function(){
+        
+    })
+
+    it('should have "raiting" pointer', async function(){
+
+    })
+
+    it('should open appropriate "movie details" page, after click on "name" field'), async function(){
+        
+    }
+})
